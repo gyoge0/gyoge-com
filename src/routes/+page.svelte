c32f8b20dc83edd5059fa8886fae55f2736486ed
Yogesh Thambidurai <gyogesh2015@outlook.com>
2023-10-12 20:51:27 -0400

    feat: add main splash page (#2)
    * chore(.idea): generate more .idea files
    
    * style: use four space indent
    
    * style: fix intellij trying to format css with two spaces
    
    * build: add unplugin-icons
    
    * ci(release-please): set release-please to bump normally
    
    * feat: add main splash page
    
    * fix: add domain configuration files to static directory

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,71 @@
+<script lang="ts">
+    import Signature from "$lib/components/Signature.svelte";
+    import { onMount } from "svelte";
+    // noinspection TypeScriptCheckImport
+    import IconEmailOutline from "virtual:icons/mdi/email-outline?raw&width=3rem&height=3rem";
+    // noinspection TypeScriptCheckImport
+    import IconGithub from "virtual:icons/mdi/github?raw&width=3rem&height=3rem";
+    import { fly } from "svelte/transition";
+
+    // svelte animations need a trigger to start
+    let start = false;
+    onMount(() => (start = true));
+</script>
+
 <main>
-  <h1>Hello World</h1>
+    <div class="animated" id="wrapper">
+        <Signature />
+    </div>
+    {#if start}
+        <ol>
+            <!-- eslint-disable svelte/no-at-html-tags -->
+            <li in:fly={{ y: 200, duration: 300, delay: 3100 }}>
+                <a href="https://github.com/gyoge0/">
+                    {@html IconGithub}
+                </a>
+            </li>
+            <li in:fly={{ y: 200, duration: 300, delay: 3200 }}>
+                <a href="mailto:yogesh@gyoge.com">
+                    {@html IconEmailOutline}
+                </a>
+            </li>
+            <!-- eslint-enable svelte/no-at-html-tags -->
+        </ol>
+    {/if}
 </main>
 
 <style>
-  main {
-    width: 100%;
-    height: 100%;
-    display: flex;
-    flex-flow: column nowrap;
-    justify-content: center;
-    align-items: center;
-  }
+    div {
+        width: 50%;
+    }
 
-  h1 {
-    font-family: sans-serif;
-  }
+    ol {
+        display: flex;
+        flex-flow: row nowrap;
+        justify-content: center;
+        align-items: center;
+        gap: 1rem;
+        color: #d1e3e1;
+    }
+
+    li,
+    a {
+        all: unset;
+    }
+
+    a:hover {
+        cursor: pointer;
+    }
+
+    main {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        height: 100%;
+        background-color: #22211f;
+        font-family: sans-serif;
+        gap: 2rem;
+    }
 </style>
