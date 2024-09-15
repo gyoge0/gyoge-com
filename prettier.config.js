56a7cc9389b07992567bb168d60d6eb8cc97fe29
Yogesh Thambidurai <yogesh@gyoge.com>
2024-09-15 15:06:23 -0400

    feat: update astro to match old website

--- prettier.config.js
+++ prettier.config.js
@@ -1,15 +1,15 @@
 export default {
-  printWidth: 100,
-  endOfLine: "auto",
-  bracketSameLine: true,
-  arrowParens: "avoid",
-  plugins: ["prettier-plugin-astro"],
-  overrides: [
-    {
-      files: "*.astro",
-      options: {
-        parser: "astro",
-      },
-    },
-  ],
+    printWidth: 100,
+    endOfLine: "auto",
+    bracketSameLine: true,
+    arrowParens: "avoid",
+    plugins: ["prettier-plugin-astro"],
+    overrides: [
+        {
+            files: "*.astro",
+            options: {
+                parser: "astro",
+            },
+        },
+    ],
 };
