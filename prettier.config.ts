7bde20ed20416d155eb9ecbd5f56e2944a649b7b
Yogesh Thambidurai <yogesh@gyoge.com>
2026-04-04 23:21:48 -0400

    style website

--- prettier.config.ts
+++ prettier.config.ts
@@ -3,6 +3,7 @@ import type { Config } from "prettier";
 const config: Config = {
     plugins: ["prettier-plugin-tailwindcss"],
     tailwindStylesheet: "./src/style.css",
+    htmlWhitespaceSensitivity: "ignore",
 };
 
 export default config;
